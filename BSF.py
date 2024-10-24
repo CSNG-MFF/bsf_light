7a5716c861b0e3d46b767ce8281797524a04adf6
dberling <david.berling@rwth-aachen.de>
2024-10-24 10:29:43 +0200

    Rm old calc_fiber, add func to repro matlab error

--- BSF.py
+++ BSF.py
@@ -141,118 +141,8 @@ def ang_conv(rho, z, func_rho_z, params):
     norm = 2 * np.pi * (rho*rho + z*z)
     return ang_conv/norm
 
-def calc_I_fiber_old(params):
-    params = calc_dependent_params(params)
-    # Calculate pencil beam of scattered
-    ## Define space
-    rho_max_pen, z_max_pen = calc_pencil_rho_z_max(
-        params['theta_div'],
-        params['xymax'],
-        params['zmax']
-    )
-    
-    if params['rho_exp_smpl']:
-        # use exp.-sampling with 0 at beginning for rho
-        rhos = np.insert(log_smplng(
-            min_=params['rhoexpmin'], 
-            max_=rho_max_pen, 
-            n_smpls=params['n_rhosmpls']-1
-        ), 0, 0)
-    else:
-        # use uniform sampling
-        rhos = np.arange(
-            0, 
-            rho_max_pen+params['rhostep'],
-            params['rhostep']
-        )
-    zs = np.arange(1, z_max_pen+params['dz'], params['dz'])
-
-    ## Define multi-path time
-    if params['tau_exp_smpl']:
-        taus = log_smplng(
-            min_=params['taumin'],
-            max_=params['taumax'],
-            n_smpls=params['n_tausmpls']
-        )
-    else:
-        taus = np.arange(
-            params['taumin'],
-            params['taumax']+params['taustep'],
-            params['taustep']
-        )
-
-    rho3_pen, z3_pen, tau3_pen = np.meshgrid(rhos, zs, taus, indexing='ij')
-    pencil_beam_scattered = pencil_scattered_time_integrated(
-        z3_pen, rho3_pen, tau3_pen, 
-        g=params['g'], 
-        mu_s=params['mu_s'], 
-        mu_a=params['mu_a'], 
-        c=params['c'], 
-        G_version=params['mu_tau']
-    )   
-
-    # create interpolator for pencil_beam:
-    interpolator_pencil_beam = Interpolator(
-        rho3_pen[:,:,0], z3_pen[:,:,0], pencil_beam_scattered, fill_value=0
-    )
-
-    # Calculate cones of light
-    ## define space
-    rhos = np.arange(
-        0, params['xymax']+params['dxy'],params['dxy']
-    )
-    zs = np.arange(
-        1, params['zmax']+params['dz'], params['dz'])
-
-    rho2_cone, z2_cone = np.meshgrid(rhos, zs, indexing='ij')
-
-    # scattered
-    cone_scattered = ang_conv(
-        rho2_cone, z2_cone, interpolator_pencil_beam.calc, 
-        params = params
-    )
-    # direct
-    cone_direct = I_direct_cone(
-        z=z2_cone, rho=rho2_cone, params=params
-    )
-    # combine
-    cone_combined = cone_scattered + cone_direct
-
-    # define final 3D space
-    xys = np.arange(
-        -params['xymax'], 
-        params['xymax']+params['dxy'], 
-        params['dxy']
-    )
-    zs = np.arange(
-        1, 
-        params['zmax']+params['dz'], 
-        params['dz']
-    )
-    xxx, yyy, zzz = np.meshgrid(xys, xys, zs, indexing='ij')
-
-    # transform into final 3D space via interpolation
-    interpolator_combined_cone = Interpolator(
-        rho2_cone, z2_cone, cone_combined
-    )
-    cone_combined_3D = interpolator_combined_cone.calc(
-        np.sqrt(xxx**2+yyy**2), zzz
-    )
-
-    # use convolution over optical fiber output to achieve final intensity profile
-    disk_conv = disk_conv_nproll(
-        intensity=cone_combined_3D, 
-        opt_radius=params['opt_radius'], 
-        dxy=params['dxy']
-    )
-    results = dict(
-        pencil = dict(rho=rho3_pen[:,:,0], z=z3_pen[:,:,0], scattered=pencil_beam_scattered),
-        cone = dict(rho=rho2_cone, z=z2_cone, scattered=cone_scattered, direct=cone_direct),
-        final = dict(x=xxx, y=yyy, z=zzz, combined=disk_conv)
-    )
-    return results
-
 def calc_I_fiber(params):
+
     params = calc_dependent_params(params)
     # Calculate pencil beam of scattered
     ## Define space
@@ -322,6 +212,116 @@ def calc_I_fiber(params):
         rho2_cone, z2_cone, interpolator_pencil_beam.calc, 
         params = params
     )
+    # create Interpolator for cone_scattered
+    interp_cone_scattered = Interpolator(rho2_cone, z2_cone, cone_scattered)
+    # disk
+    disk_scattered = disk_conv_numpy(
+        rho=rho2_cone, 
+        z=z2_cone, 
+        I_rho_z=interp_cone_scattered.calc, 
+        opt_radius=params['opt_radius'], 
+        dxy=params['dxy_scattered_disk']
+    )
+    # Calculate direct light
+    def I_direct_cone_fixed_params(rho, z):
+        return I_direct_cone(z, rho, params)
+    disk_direct = disk_conv_numpy(
+        rho=rho2_cone, 
+        z=z2_cone, 
+        I_rho_z=I_direct_cone_fixed_params, 
+        opt_radius=params['opt_radius'], 
+        dxy=params['dxy_direct_disk']
+    )
+    
+    # results
+    results = dict(
+        pencil = dict(rho=rho3_pen[:,:,0], z=z3_pen[:,:,0], scattered=pencil_beam_scattered),
+        cone = dict(rho=rho2_cone, z=z2_cone, scattered=cone_scattered),
+        final = dict(rho=rho2_cone, z=z2_cone, scattered=disk_scattered, direct=disk_direct, combined=disk_direct+disk_scattered)
+    )
+    return results
+
+def calc_I_fiber_reproduce_error(params, fix_pencil_rho_z=False, BSF_error=False):
+
+    params = calc_dependent_params(params)
+    # Calculate pencil beam of scattered
+    ## Define space
+    rho_max_pen, z_max_pen = calc_pencil_rho_z_max(
+        params['theta_div'],
+        params['xymax'],
+        params['zmax']
+    )
+    if fix_pencil_rho_z:
+        rho_max_pen = fix_pencil_rho_z[0]
+        z_max_pen = fix_pencil_rho_z[1]
+    
+    if params['rho_exp_smpl']:
+        # use exp.-sampling with 0 at beginning for rho
+        rhos = np.insert(log_smplng(
+            min_=params['rhoexpmin'], 
+            max_=rho_max_pen, 
+            n_smpls=params['n_rhosmpls']-1
+        ), 0, 0)
+    else:
+        # use uniform sampling
+        rhos = np.arange(
+            0, 
+            rho_max_pen+params['rhostep'],
+            params['rhostep']
+        )
+    zs = np.arange(1, z_max_pen+params['dz'], params['dz'])
+
+    ## Define multi-path time
+    if params['tau_exp_smpl']:
+        taus = log_smplng(
+            min_=params['taumin'],
+            max_=params['taumax'],
+            n_smpls=params['n_tausmpls']
+        )
+    else:
+        taus = np.arange(
+            params['taumin'],
+            params['taumax']+params['taustep'],
+            params['taustep']
+        )
+
+    rho3_pen, z3_pen, tau3_pen = np.meshgrid(rhos, zs, taus, indexing='ij')
+    pencil_beam_scattered = pencil_scattered_time_integrated(
+        z3_pen, rho3_pen, tau3_pen, 
+        g=params['g'], 
+        mu_s=params['mu_s'], 
+        mu_a=params['mu_a'], 
+        c=params['c'], 
+        G_version=params['mu_tau']
+    )   
+
+    # create interpolator for pencil_beam:
+    if BSF_error == True:
+        interpolator_pencil_beam = Interpolator(
+            rho3_pen[:,:,0], z3_pen[:,:,0], pencil_beam_scattered, fill_value=np.nan
+        )
+    else:
+        interpolator_pencil_beam = Interpolator(
+            rho3_pen[:,:,0], z3_pen[:,:,0], pencil_beam_scattered, fill_value=0
+        )
+
+    # Calculate scattered light
+    ## define space
+    rhos = np.arange(
+        0, params['xymax']+params['dxy'],params['dxy']
+    )
+    zs = np.arange(
+        1, params['zmax']+params['dz'], params['dz'])
+
+    rho2_cone, z2_cone = np.meshgrid(rhos, zs, indexing='ij')
+
+    # cone
+    cone_scattered = ang_conv(
+        rho2_cone, z2_cone, interpolator_pencil_beam.calc, 
+        params = params
+    )
+    if BSF_error == True:
+        cone_scattered[np.isnan(cone_scattered)] = 0
     # create Interpolator for cone_scattered
     interp_cone_scattered = Interpolator(rho2_cone, z2_cone, cone_scattered)
     # disk
