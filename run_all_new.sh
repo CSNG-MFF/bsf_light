01b66113b1f0403c9a6d43f0793408f7b62514df
dberling <david.berling@rwth-aachen.de>
2024-10-27 10:18:24 +0100

    Config cluster run

--- run_all_new.sh
+++ run_all_new.sh
@@ -2,7 +2,11 @@
 
 python run.py 'params/default_lowRAM.yml' 'results/default_lowRAM.pickle'
 
-#python run.py 'params/default_Lutomirski.yml' 'results/default_Lutomirski.pickle'
+python run.py 'params/default.yml' 'results/default.pickle'
 
-#python run.py 'params/default.yml' 'results/default.pickle'
+python run.py 'params/default_Lutomirski.yml' 'results/default_Lutomirski.pickle'
+
+python run.py 'params/default_vandeHulst.yml' 'results/default_vandeHulst.pickle'
+
+python run.py 'params/error.yml' 'results/error.pickle'
 
---
+++ run_error.py
@@ -0,0 +1,15 @@
+import sys
+from BSF import calc_I_fiber
+from load_save_utils import load_yaml, save_pickle
+from time import time
+start = time()
+results = calc_I_fiber_reproduce_error(
+        load_yaml(str(sys.argv[1])),
+        fix_pencil_rho_z=(500,750),
+        ang_conv_fill_nan=True,
+        decrease_disk_conv_volume_radially=100
+)
+end = time()
+results['comp_time_s'] = end - start
+
+save_pickle(results, str(sys.argv[2]))
