c892e7519073fb1ef0aa280c23acc1a2e3d1e6e2
dberling <david.berling@rwth-aachen.de>
2024-11-05 07:43:53 +0100

    Track RAM consumption

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import resource
 import sys
 from BSF import calc_I_fiber
 from load_save_utils import load_yaml, save_pickle
@@ -6,5 +7,6 @@ start = time()
 results = calc_I_fiber(load_yaml(str(sys.argv[1])))
 end = time()
 results['comp_time_s'] = end - start
+results['MaxRS_KB'] = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
 save_pickle(results, str(sys.argv[2]))
