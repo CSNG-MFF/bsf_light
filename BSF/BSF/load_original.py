af2e034a266c1b23e5486a1bfeae00f3eff24b9e
dberling <david.berling@rwth-aachen.de>
2024-11-11 16:01:15 +0100

    Adapt code to restucturing of original data

--- BSF/BSF/load_original.py
+++ BSF/BSF/load_original.py
@@ -25,8 +25,8 @@ def load_published_exp_data():
     Spatial units all [um], light transmission decimals.
     """
     ## import scraped exp data from Fig 5a (decay over depth)
-    depth_data = pd.read_csv('data_from_paper/2205_David_a_points.txt', names=['x','y'], delimiter=';')
-    depth_data_err = pd.read_csv('data_from_paper/2205_David_a_err.txt', names=['x','y'], delimiter=';')
+    depth_data = pd.read_csv('published_original/Fig5a_points.txt', names=['x','y'], delimiter=';')
+    depth_data_err = pd.read_csv('published_original/Fig5a_err.txt', names=['x','y'], delimiter=';')
     depth = dict()
     # sort errors and substract datapoint to adapt for plt.errorbar arguments
     depth['z'] = depth_data.x.values*1000 # convert from mm to um
@@ -34,8 +34,8 @@ def load_published_exp_data():
     depth['transmission_err'] = reformat_error_data(depth_data, depth_data_err)/100 # convert from percent to decimal
     
     ## import scraped exp data from Fig 5b (radial profiles at z=300um, 600 um)
-    radial_data_z600 = pd.read_csv('data_from_paper/2205_David_b_points_red.txt', names=['x','y'], delimiter=';')
-    radial_data_z300 = pd.read_csv('data_from_paper/2205_David_b_points_blue.txt', names=['x','y'], delimiter=';')
+    radial_data_z600 = pd.read_csv('published_original/Fig5b_points_red.txt', names=['x','y'], delimiter=';')
+    radial_data_z300 = pd.read_csv('published_original/Fig5b_points_blue.txt', names=['x','y'], delimiter=';')
     radial = dict()
     radial['x_z300'] = radial_data_z300.x.values*1000 # convert from mm to um
     radial['transmission_z300'] = radial_data_z300.y.values/100 # convert from percent to decimal
@@ -50,15 +50,15 @@ def load_published_model_data():
 
     Spatial units all [um], light transmission decimals.
     """
-    depth_curve = pd.read_csv('data_from_paper/depth_profile.csv')
+    depth_curve = pd.read_csv('published_original/depth_profile.csv')
     depth = dict()
     depth['z'] = depth_curve['x'].values * 1e3
     depth['transmission'] = depth_curve['transmission'].values * 1e-2
 
     
     ## import model prediction from Fig 5b
-    radial_curve_z300 = np.load('data_from_paper/BSF_model_radial_curve_300.npy')
-    radial_curve_z600 = np.load('data_from_paper/BSF_model_radial_curve_600.npy')
+    radial_curve_z300 = np.load('published_original/BSF_model_radial_curve_300.npy')
+    radial_curve_z600 = np.load('published_original/BSF_model_radial_curve_600.npy')
     radial = dict()
     radial['x_z300'] = radial_curve_z300[:,0]*1000 # convert from mm to um
     radial['transmission_z300'] = radial_curve_z300[:,1]/100 # convert from percent to decimal
@@ -68,7 +68,7 @@ def load_published_model_data():
     return depth, radial
 
 def load_matlab_model_data():
-    matlab_data = loadmat('matlab/output3D_26_08_2024.mat')['out']
+    matlab_data = loadmat('matlab_original/output3D_26_08_2024.mat')['out']
     matlab_data_xz = matlab_data[int(matlab_data.shape[0]/2):,int(matlab_data.shape[1]/2),:]
     matlab_dx = 5
     data = dict()
