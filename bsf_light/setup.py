d057cc271fc08d7d7fee44263d669de422eb88eb
dberling <david.berling@rwth-aachen.de>
2024-11-20 14:32:59 +0100

    Prepare 1.0.0 release at PyPI

--- bsf_light/setup.py
+++ bsf_light/setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='bsf_light',
-    version='1.0',
+    version='1.0.0',
     description='Beams Simple and Fast LIGHT simulation (bsf_light) - Fast light simulation for brain tissue using a beam-spread-function approach',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='David Berling',
     author_email='berling@ksvi.mff.cuni.cz',
     packages=find_packages(),
+    python_requires=">=3.13",
     install_requires=[
-        'numpy', 
-        'scipy',
-        'pyyaml',
-        'matplotlib'
+        'numpy>=2.1.3', 
+        'scipy>=1.14.1',
+        'pyyaml>=6.0.2',
+        'matplotlib>=3.9.2'
     ],
     test_suite='pytest',
     tests_require=['pytest'],
